390fdd8514e9ddedef0f0e15809bdc7b59a9be38
kruskal <99559985+kruskall@users.noreply.github.com>
2023-02-26 22:39:12 +0100

    feat: add kakfa producer

--- kafka/producer.go
+++ kafka/producer.go
@@ -17,4 +17,198 @@
 
 package kafka
 
-// TODO
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+
+	"go.uber.org/zap"
+
+	"github.com/twmb/franz-go/pkg/kgo"
+	"github.com/twmb/franz-go/plugin/kzap"
+
+	"github.com/elastic/apm-data/model"
+)
+
+// ProducerConfig holds configuration for publishing events to Kafka.
+type ProducerConfig struct {
+	// Broker holds the (host:port) address of the Kafka broker to which
+	// events should be published.
+	Broker string
+
+	// ClientID to use when connecting to Kafka. This is used for logging
+	// and client identification purposes.
+	ClientID string
+	// Version is the software version to use in the Kafka client. This is
+	// useful since it shows up in Kafka metrics and logs.
+	Version string
+
+	// TopicPrefix holds the name of the Kafka topic prefix to which events
+	// should be published. The producer will send messages to 5 different
+	// topics using the <TopicPrefix>.<EventType> format.
+	TopicPrefix string
+
+	// Logger is used for logging producer errors.
+	Logger *zap.Logger
+
+	// Key, if non-nil, is used to set the key for an event message.
+	//
+	// Producer uses murmur2 partitioning: consistent hashing over the
+	// message key, if it is non-nil; random partition assignment if the
+	// message has no key.
+	//
+	// If Key is nil, the message key will be set to the event's trace ID
+	// if there is one, and nil otherwise.
+	Key func(model.APMEvent) []byte
+
+	// TODO(axw) Kafka credentials
+
+	// Kafka topics to publish to.
+	txTopic     string
+	spanTopic   string
+	metricTopic string
+	errorTopic  string
+	logTopic    string
+}
+
+// Validate checks that cfg is valid, and returns an error otherwise.
+func (cfg ProducerConfig) Validate() error {
+	if cfg.Broker == "" {
+		return errors.New("broker cannot be empty")
+	}
+	if cfg.TopicPrefix == "" {
+		return errors.New("topic cannot be empty")
+	}
+	if cfg.Logger == nil {
+		return errors.New("logger cannot be nil")
+	}
+	return nil
+}
+
+// Producer is a model.BatchProcessor that publishes events to Kafka.
+type Producer struct {
+	cfg    ProducerConfig
+	client *kgo.Client
+
+	mu sync.RWMutex
+}
+
+// NewProducer returns a new Producer with the given config.
+func NewProducer(cfg ProducerConfig) (*Producer, error) {
+	if err := cfg.Validate(); err != nil {
+		return nil, fmt.Errorf("invalid producer config: %w", err)
+	}
+
+	opts := []kgo.Opt{
+		kgo.SeedBrokers(cfg.Broker),
+		kgo.WithLogger(kzap.New(cfg.Logger)),
+	}
+	if cfg.ClientID != "" {
+		opts = append(opts, kgo.ClientID(cfg.ClientID))
+		if cfg.Version != "" {
+			opts = append(opts, kgo.SoftwareNameAndVersion(
+				cfg.ClientID, cfg.Version,
+			))
+		}
+	}
+	// TODO(marclop) block on re-balances, auto-commit high watermarks.
+	client, err := kgo.NewClient(opts...)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating producer: %w", err)
+	}
+	// Issue a metadata refresh request on construction, so the broker list is
+	// populated.
+	client.ForceMetadataRefresh()
+
+	cfg.txTopic = fmt.Sprintf("%s.transaction", cfg.TopicPrefix)
+	cfg.spanTopic = fmt.Sprintf("%s.span", cfg.TopicPrefix)
+	cfg.metricTopic = fmt.Sprintf("%s.metric", cfg.TopicPrefix)
+	cfg.errorTopic = fmt.Sprintf("%s.error", cfg.TopicPrefix)
+	cfg.logTopic = fmt.Sprintf("%s.log", cfg.TopicPrefix)
+
+	return &Producer{
+		cfg:    cfg,
+		client: client,
+	}, nil
+}
+
+// Close stops the producer
+func (p *Producer) Close() error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.client.Close()
+	return nil
+}
+
+// ProjectIDKey represents the key for the project ID set in the context.
+type ProjectIDKey struct{}
+
+// ProcessBatch publishes the events in batch to the specified Kafka topic.
+func (p *Producer) ProcessBatch(ctx context.Context, batch *model.Batch, wg *sync.WaitGroup) error {
+	projectID, ok := ctx.Value(ProjectIDKey{}).(string)
+	if !ok {
+		return errors.New("project ID missing")
+	}
+
+	// Take a read lock to prevent Close from closing the input channel
+	// while we're attempting to send to it.
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+
+	wg.Add(len(*batch))
+
+	for _, event := range *batch {
+		record := &kgo.Record{
+			Headers: []kgo.RecordHeader{{
+				Key:   "project_id",
+				Value: []byte(projectID),
+			}},
+		}
+
+		switch event.Processor {
+		case model.TransactionProcessor:
+			record.Topic = p.cfg.txTopic
+		case model.SpanProcessor:
+			record.Topic = p.cfg.spanTopic
+		case model.ErrorProcessor:
+			record.Topic = p.cfg.errorTopic
+		case model.MetricsetProcessor:
+			record.Topic = p.cfg.metricTopic
+		case model.LogProcessor:
+			record.Topic = p.cfg.logTopic
+		}
+
+		encoded, err := event.MarshalJSON()
+		if err != nil {
+			return err
+		}
+
+		record.Value = encoded
+		if p.cfg.Key != nil {
+			record.Key = p.cfg.Key(event)
+		} else if event.Trace.ID != "" {
+			// Set Key to Trace.ID if there is one. If there isn't,
+			// the message will be sent to a random partition.
+			record.Key = []byte(event.Trace.ID)
+		}
+
+		p.client.Produce(ctx, record, func(msg *kgo.Record, err error) {
+			defer wg.Done()
+			if err != nil {
+				p.cfg.Logger.Error("failed producing message",
+					zap.Error(err),
+					zap.String("topic", msg.Topic),
+				)
+			}
+		})
+	}
+	return nil
+}
+
+func (p *Producer) Healthy() error {
+	if brokers := p.client.DiscoveredBrokers(); len(brokers) < 1 {
+		return fmt.Errorf("number of active brokers below 1")
+	}
+	return nil
+}
