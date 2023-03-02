f0bd5b0f0968521c261cb14ff62aa3f0ddc19f61
Marc Lopez Rubio <marc5.12@outlook.com>
2023-03-02 15:47:40 +0800

    pubsublite(consumer): Receive from N subscriptions
    Adds support for receiving messages from multiple subscriptions at once
    with the same configuration.
    
    Signed-off-by: Marc Lopez Rubio <marc5.12@outlook.com>

--- pubsublite/consumer.go
+++ pubsublite/consumer.go
@@ -26,6 +26,7 @@ import (
 	"cloud.google.com/go/pubsub"
 	"cloud.google.com/go/pubsublite/pscompat"
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/api/option"
 
 	"github.com/elastic/apm-data/model"
@@ -41,8 +42,8 @@ type Decoder interface {
 
 // ConsumerConfig defines the configuration for the PubSub Lite consumer.
 type ConsumerConfig struct {
-	// PubSub Lite subscription.
-	Subscription Subscription
+	// PubSub Lite subscriptions.
+	Subscriptions []Subscription
 	// Decoder holds an encoding.Decoder for decoding events.
 	Decoder Decoder
 	// Logger to use for any errors.
@@ -89,8 +90,15 @@ func (s Subscription) Validate() error {
 // Validate ensures the configuration is valid, otherwise, returns an error.
 func (cfg ConsumerConfig) Validate() error {
 	var errs []error
-	if err := cfg.Subscription.Validate(); err != nil {
-		errs = append(errs, err)
+	if len(cfg.Subscriptions) == 0 {
+		errs = append(errs,
+			errors.New("pubsublite: at least one subscription must be set"),
+		)
+	}
+	for i, subscription := range cfg.Subscriptions {
+		if err := subscription.Validate(); err != nil {
+			errs = append(errs, fmt.Errorf("%d: %w", i, err))
+		}
 	}
 	if cfg.Decoder == nil {
 		errs = append(errs, errors.New("pubsublite: decoder must be set"))
@@ -110,12 +118,13 @@ func (cfg ConsumerConfig) Validate() error {
 	return errors.Join(errs...)
 }
 
-// Consumer receives PubSub Lite messages from an existing subscription. The
-// underlying library processes messages concurrently for each partition.
+// Consumer receives PubSub Lite messages from a existing subscription(s). The
+// underlying library processes messages concurrently per subscription and
+// partition.
 type Consumer struct {
 	mu             sync.Mutex
 	cfg            ConsumerConfig
-	consumer       *pscompat.SubscriberClient
+	consumers      []consumer
 	stopSubscriber context.CancelFunc
 }
 
@@ -124,7 +133,6 @@ func NewConsumer(ctx context.Context, cfg ConsumerConfig) (*Consumer, error) {
 	if err := cfg.Validate(); err != nil {
 		return nil, err
 	}
-	cfg.Logger = cfg.Logger.With(zap.String("subscription", cfg.Subscription.String()))
 	settings := pscompat.ReceiveSettings{
 		// Pub/Sub Lite does not have a concept of 'nack'. If the nack handler
 		// implementation returns nil, the message is acknowledged. If an error
@@ -143,15 +151,29 @@ func NewConsumer(ctx context.Context, cfg ConsumerConfig) (*Consumer, error) {
 			return nil // nil is returned to avoid terminating the subscriber.
 		},
 	}
-	consumer, err := pscompat.NewSubscriberClientWithSettings(
-		ctx, cfg.Subscription.String(), settings, cfg.ClientOpts...,
-	)
-	if err != nil {
-		return nil, err
+	consumers := make([]consumer, 0, len(cfg.Subscriptions))
+	for _, subscription := range cfg.Subscriptions {
+		client, err := pscompat.NewSubscriberClientWithSettings(
+			ctx, subscription.String(), settings, cfg.ClientOpts...,
+		)
+		if err != nil {
+			return nil, err
+		}
+		consumers = append(consumers, consumer{
+			SubscriberClient: client,
+			delivery:         cfg.Delivery,
+			processor:        cfg.Processor,
+			decoder:          cfg.Decoder,
+			logger: cfg.Logger.With(
+				zap.String("subscription", subscription.Name),
+				zap.String("region", subscription.Region),
+				zap.String("project", subscription.Project),
+			),
+		})
 	}
 	return &Consumer{
-		cfg:      cfg,
-		consumer: consumer,
+		cfg:       cfg,
+		consumers: consumers,
 	}, nil
 }
 
@@ -169,53 +191,71 @@ func (c *Consumer) Run(ctx context.Context) error {
 	c.mu.Lock()
 	if c.stopSubscriber != nil {
 		c.mu.Unlock()
-		return errors.New("consumer already started")
+		return errors.New("pubsublite: consumer already started")
 	}
 	ctx, c.stopSubscriber = context.WithCancel(ctx)
 	c.mu.Unlock()
-	return c.consumer.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
-		var event model.APMEvent
-		if err := c.cfg.Decoder.Decode(msg.Data, &event); err != nil {
-			defer msg.Nack()
-			partition, offset := partitionOffset(msg.ID)
-			c.cfg.Logger.Error("unable to decode message.Data into model.APMEvent",
-				zap.Error(err),
-				zap.ByteString("message.value", msg.Data),
-				zap.Int64("offset", offset),
-				zap.Int("partition", partition),
-			)
-			return
-		}
-		batch := model.Batch{event}
-		ctx = queuecontext.WithMetadata(ctx, msg.Attributes)
-		var err error
-		switch c.cfg.Delivery {
-		case apmqueue.AtMostOnceDeliveryType:
-			msg.Ack()
-		case apmqueue.AtLeastOnceDeliveryType:
-			defer func() {
-				if err != nil {
-					msg.Nack()
-				} else {
-					msg.Ack()
-				}
-			}()
-		}
-		if err = c.cfg.Processor.ProcessBatch(ctx, &batch); err != nil {
-			partition, offset := partitionOffset(msg.ID)
-			c.cfg.Logger.Error("unable to process event",
-				zap.Error(err),
-				zap.Int64("offset", offset),
-				zap.Int("partition", partition),
-			)
-			return
-		}
-	})
+	g, ctx := errgroup.WithContext(ctx)
+	for _, consumer := range c.consumers {
+		consumer := consumer
+		g.Go(func() error {
+			return consumer.Receive(ctx, consumer.processMessage)
+		})
+	}
+	return g.Wait()
 }
 
 // Healthy returns an error if the consumer isn't healthy.
 func (c *Consumer) Healthy() error {
 	return nil // TODO(marclop)
+}
+
+// consumer wraps a PubSub Lite SubscriberClient.
+type consumer struct {
+	*pscompat.SubscriberClient
+	logger    *zap.Logger
+	delivery  apmqueue.DeliveryType
+	processor model.BatchProcessor
+	decoder   Decoder
+}
+
+func (c consumer) processMessage(ctx context.Context, msg *pubsub.Message) {
+	var event model.APMEvent
+	if err := c.decoder.Decode(msg.Data, &event); err != nil {
+		defer msg.Nack()
+		partition, offset := partitionOffset(msg.ID)
+		c.logger.Error("unable to decode message.Data into model.APMEvent",
+			zap.Error(err),
+			zap.ByteString("message.value", msg.Data),
+			zap.Int64("offset", offset),
+			zap.Int("partition", partition),
+		)
+		return
+	}
+	batch := model.Batch{event}
+	ctx = queuecontext.WithMetadata(ctx, msg.Attributes)
+	var err error
+	switch c.delivery {
+	case apmqueue.AtMostOnceDeliveryType:
+		msg.Ack()
+	case apmqueue.AtLeastOnceDeliveryType:
+		defer func() {
+			if err != nil {
+				msg.Nack()
+			} else {
+				msg.Ack()
+			}
+		}()
+	}
+	if err = c.processor.ProcessBatch(ctx, &batch); err != nil {
+		partition, offset := partitionOffset(msg.ID)
+		c.logger.Error("unable to process event",
+			zap.Error(err),
+			zap.Int64("offset", offset),
+			zap.Int("partition", partition),
+		)
+		return
+	}
 }
 
 // Parses the message partition and offset. If the metadata can't be parsed,
