5d771f0b48fc7f335f9ede402f2bd2acf27d7058
Marc Lopez Rubio <marc5.12@outlook.com>
2023-05-16 07:44:20 +0800

    codec: Support JSON encoding / decoding metrics (#140)
    Adds support to record encoding and decoding bytes in an injected int64
    counter.
    
    Signed-off-by: Marc Lopez Rubio <marc5.12@outlook.com>

--- codec/json/json.go
+++ codec/json/json.go
@@ -19,20 +19,47 @@
 package json
 
 import (
+	"context"
 	"encoding/json"
+
+	"go.opentelemetry.io/otel/metric"
 
 	"github.com/elastic/apm-data/model"
 )
 
 // JSON wraps the standard json library.
-type JSON struct{}
+type JSON struct {
+	encoded metric.Int64Counter
+	decoded metric.Int64Counter
+}
 
 // Encode accepts a model.APMEvent and returns the encoded JSON representation.
 func (e JSON) Encode(in model.APMEvent) ([]byte, error) {
-	return json.Marshal(in)
+	b, err := json.Marshal(in)
+	if e.encoded != nil {
+		e.encoded.Add(context.Background(), int64(len(b)))
+	}
+	return b, err
 }
 
 // Decode decodes an encoded model.APM Event into its struct form.
 func (e JSON) Decode(in []byte, out *model.APMEvent) error {
+	if e.decoded != nil {
+		e.decoded.Add(context.Background(), int64(len(in)))
+	}
 	return json.Unmarshal(in, out)
+}
+
+// RecordEncodedBytes configures the passed JSON codec to record the number of
+// bytes that have been encoded into JSON.
+func RecordEncodedBytes(c *JSON, m metric.Int64Counter) *JSON {
+	c.encoded = m
+	return c
+}
+
+// RecordDecodedBytes configures the passed JSON codec to record the number of
+// bytes that have been decoded.
+func RecordDecodedBytes(c *JSON, m metric.Int64Counter) *JSON {
+	c.decoded = m
+	return c
 }
