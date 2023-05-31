325a6f8b0db8a179f55975c5390ff7f10465e9d7
Andrew Wilkins <axw@elastic.co>
2023-05-31 14:33:51 +0800

    kafka: create separate TopicCreator type
    This allows one to use a singke Manager to
    create topics with differing partition counts
    and configs.

--- kafka/manager.go
+++ kafka/manager.go
@@ -35,19 +35,6 @@ import (
 // ManagerConfig holds configuration for managing Kafka topics.
 type ManagerConfig struct {
 	CommonConfig
-
-	// PartitionCount is the number of partitions to assign to
-	// newly created topics.
-	//
-	// Must be non-zero. If PartitonCount is -1, the broker's
-	// default value (requires Kafka 2.4+).
-	PartitionCount int
-
-	// TopicConfigs holds any topic configs to assign to newly
-	// created topics, such as `retention.ms`.
-	//
-	// See https://kafka.apache.org/documentation/#topicconfigs
-	TopicConfigs map[string]string
 }
 
 // finalize ensures the configuration is valid, setting default values from
@@ -58,18 +45,14 @@ func (cfg *ManagerConfig) finalize() error {
 	if err := cfg.CommonConfig.finalize(); err != nil {
 		errs = append(errs, err)
 	}
-	if cfg.PartitionCount == 0 {
-		errs = append(errs, errors.New("kafka: partition count must be non-zero"))
-	}
 	return errors.Join(errs...)
 }
 
 // Manager manages Kafka topics.
 type Manager struct {
-	cfg          ManagerConfig
-	topicConfigs map[string]*string
-	client       *kadm.Client
-	tracer       trace.Tracer
+	cfg    ManagerConfig
+	client *kadm.Client
+	tracer trace.Tracer
 }
 
 // NewManager returns a new Manager with the given config.
@@ -77,22 +60,14 @@ func NewManager(cfg ManagerConfig) (*Manager, error) {
 	if err := cfg.finalize(); err != nil {
 		return nil, fmt.Errorf("kafka: invalid manager config: %w", err)
 	}
-	var topicConfigs map[string]*string
-	if len(cfg.TopicConfigs) != 0 {
-		topicConfigs = make(map[string]*string, len(cfg.TopicConfigs))
-		for k, v := range cfg.TopicConfigs {
-			topicConfigs[k] = kadm.StringPtr(v)
-		}
-	}
 	client, err := cfg.newClient()
 	if err != nil {
 		return nil, fmt.Errorf("kafka: failed creating kafka client: %w", err)
 	}
 	return &Manager{
-		cfg:          cfg,
-		topicConfigs: topicConfigs,
-		client:       kadm.NewClient(client),
-		tracer:       cfg.tracerProvider().Tracer("kafka"),
+		cfg:    cfg,
+		client: kadm.NewClient(client),
+		tracer: cfg.tracerProvider().Tracer("kafka"),
 	}, nil
 }
 
@@ -101,63 +76,6 @@ func NewManager(cfg ManagerConfig) (*Manager, error) {
 func (m *Manager) Close() error {
 	m.client.Close()
 	return nil
-}
-
-// CreateTopics creates one or more topics.
-//
-// Topics that already exist will be left unmodified.
-func (m *Manager) CreateTopics(ctx context.Context, topics ...apmqueue.Topic) error {
-	// TODO(axw) how should we record topics?
-	ctx, span := m.tracer.Start(ctx, "CreateTopics", trace.WithAttributes(
-		semconv.MessagingSystemKey.String("kafka"),
-	))
-	defer span.End()
-
-	topicNames := make([]string, len(topics))
-	for i, topic := range topics {
-		topicNames[i] = string(topic)
-	}
-	responses, err := m.client.CreateTopics(
-		ctx,
-		int32(m.cfg.PartitionCount),
-		-1, // default.replication.factor
-		m.topicConfigs,
-		topicNames...,
-	)
-	if err != nil {
-		span.RecordError(err)
-		span.SetStatus(codes.Error, "CreateTopics returned an error")
-		return fmt.Errorf("failed to create kafka topics: %w", err)
-	}
-	createTopicParamsFields := []zap.Field{
-		zap.Int("partition_count", m.cfg.PartitionCount),
-	}
-	if m.cfg.TopicConfigs != nil {
-		createTopicParamsFields = append(createTopicParamsFields,
-			zap.Reflect("topic_configs", m.cfg.TopicConfigs),
-		)
-	}
-	var createErrors []error
-	for _, response := range responses.Sorted() {
-		logger := m.cfg.Logger.With(zap.String("topic", response.Topic))
-		if err := response.Err; err != nil {
-			if errors.Is(err, kerr.TopicAlreadyExists) {
-				logger.Debug("kafka topic already exists")
-			} else {
-				span.RecordError(err)
-				span.SetStatus(codes.Error, "failed to create one or more topic")
-				createErrors = append(createErrors,
-					fmt.Errorf(
-						"failed to create topic %q: %w",
-						response.Topic, err,
-					),
-				)
-			}
-			continue
-		}
-		logger.Info("created kafka topic", createTopicParamsFields...)
-	}
-	return errors.Join(createErrors...)
 }
 
 // DeleteTopics deletes one or more topics.
---
+++ kafka/topiccreator.go
@@ -0,0 +1,145 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package kafka
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/twmb/franz-go/pkg/kadm"
+	"github.com/twmb/franz-go/pkg/kerr"
+	"go.opentelemetry.io/otel/codes"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
+
+	apmqueue "github.com/elastic/apm-queue"
+)
+
+// TopicCreatorConfig holds configuration for creating Kafka topics.
+type TopicCreatorConfig struct {
+	// PartitionCount is the number of partitions to assign to
+	// newly created topics.
+	//
+	// Must be non-zero. If PartitonCount is -1, the broker's
+	// default value (requires Kafka 2.4+).
+	PartitionCount int
+
+	// TopicConfigs holds any topic configs to assign to newly
+	// created topics, such as `retention.ms`.
+	//
+	// See https://kafka.apache.org/documentation/#topicconfigs
+	TopicConfigs map[string]string
+}
+
+// finalize ensures the configuration is valid, setting default values from
+// environment variables as described in doc comments, returning an error if
+// any configuration is invalid.
+func (cfg TopicCreatorConfig) Validate() error {
+	var errs []error
+	if cfg.PartitionCount == 0 {
+		errs = append(errs, errors.New("kafka: partition count must be non-zero"))
+	}
+	return errors.Join(errs...)
+}
+
+// TopicCreator creates Kafka topics.
+type TopicCreator struct {
+	m                *Manager
+	partitionCount   int
+	origTopicConfigs map[string]string
+	topicConfigs     map[string]*string
+}
+
+// NewTopicCreator returns a new TopicCreator with the given config.
+func (m *Manager) NewTopicCreator(cfg TopicCreatorConfig) (*TopicCreator, error) {
+	if err := cfg.Validate(); err != nil {
+		return nil, fmt.Errorf("kafka: invalid topic creator config: %w", err)
+	}
+	var topicConfigs map[string]*string
+	if len(cfg.TopicConfigs) != 0 {
+		topicConfigs = make(map[string]*string, len(cfg.TopicConfigs))
+		for k, v := range cfg.TopicConfigs {
+			topicConfigs[k] = kadm.StringPtr(v)
+		}
+	}
+	return &TopicCreator{
+		m:                m,
+		partitionCount:   cfg.PartitionCount,
+		origTopicConfigs: cfg.TopicConfigs,
+		topicConfigs:     topicConfigs,
+	}, nil
+}
+
+// CreateTopics creates one or more topics.
+//
+// Topics that already exist will be left unmodified.
+func (c *TopicCreator) CreateTopics(ctx context.Context, topics ...apmqueue.Topic) error {
+	// TODO(axw) how should we record topics?
+	ctx, span := c.m.tracer.Start(ctx, "CreateTopics", trace.WithAttributes(
+		semconv.MessagingSystemKey.String("kafka"),
+	))
+	defer span.End()
+
+	topicNames := make([]string, len(topics))
+	for i, topic := range topics {
+		topicNames[i] = string(topic)
+	}
+	responses, err := c.m.client.CreateTopics(
+		ctx,
+		int32(c.partitionCount),
+		-1, // default.replication.factor
+		c.topicConfigs,
+		topicNames...,
+	)
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "CreateTopics returned an error")
+		return fmt.Errorf("failed to create kafka topics: %w", err)
+	}
+	createTopicParamsFields := []zap.Field{
+		zap.Int("partition_count", c.partitionCount),
+	}
+	if c.origTopicConfigs != nil {
+		createTopicParamsFields = append(createTopicParamsFields,
+			zap.Reflect("topic_configs", c.origTopicConfigs),
+		)
+	}
+	var createErrors []error
+	for _, response := range responses.Sorted() {
+		logger := c.m.cfg.Logger.With(zap.String("topic", response.Topic))
+		if err := response.Err; err != nil {
+			if errors.Is(err, kerr.TopicAlreadyExists) {
+				logger.Debug("kafka topic already exists")
+			} else {
+				span.RecordError(err)
+				span.SetStatus(codes.Error, "failed to create one or more topic")
+				createErrors = append(createErrors,
+					fmt.Errorf(
+						"failed to create topic %q: %w",
+						response.Topic, err,
+					),
+				)
+			}
+			continue
+		}
+		logger.Info("created kafka topic", createTopicParamsFields...)
+	}
+	return errors.Join(createErrors...)
+}
