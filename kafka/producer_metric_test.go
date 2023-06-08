1d5d8763f683136c3c1bd9d9f481b39366893128
Edoardo Tenani <edoardo.tenani@elastic.co>
2023-06-08 15:37:56 +0200

    refactor tests to remove flakiness
    I refactored the test to:
    - identify the correct ScopeMetrics to target for assertions
    - use separate readers to avoid compounding collected metrics
    - extracted common logic to helper functions
    - test only the specific attributes we are intested in without using
    go-cmp
    
    I decided to remove go-cmp comparator as it was not providing much value
    once I refactored the test to target the specific fields I was
    interested in.
    
    As these test are not flaky anymore I also removed the metrics build
    tag, as is no more useful.

--- kafka/producer_metric_test.go
+++ kafka/producer_metric_test.go
@@ -15,9 +15,6 @@
 // specific language governing permissions and limitations
 // under the License.
 
-//go:build metrics
-// +build metrics
-
 package kafka
 
 import (
@@ -25,10 +22,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/otel"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 	"go.opentelemetry.io/otel/trace"
@@ -42,11 +37,89 @@ import (
 // run this test with:
 // go test --tags=metrics -run
 // go test -v -run TestProducerMetrics --tags=metrics
-func TestProducerMetrics(t *testing.T) {
+func TestProducerMetrics_deadlineExceeded(t *testing.T) {
+	producer, rdr := setupTestProducer(t)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 0)
+	defer cancel()
+
+	err := producer.ProcessBatch(ctx, &model.Batch{
+		model.APMEvent{Transaction: &model.Transaction{ID: "1"}},
+		model.APMEvent{Transaction: &model.Transaction{ID: "2"}},
+		model.APMEvent{Span: &model.Span{ID: "3"}},
+	})
+	assert.NoError(t, err)
+
+	var rm metricdata.ResourceMetrics
+	assert.NoError(t, rdr.Collect(context.Background(), &rm))
+
+	metrics := extractMetrics(rm.ScopeMetrics)
+	assert.Len(t, metrics, 1)
+	metric := metrics[0]
+
+	want := metricdata.Metrics{
+		Name: "write.error",
+		Data: metricdata.Sum[int64]{
+			DataPoints: []metricdata.DataPoint[int64]{
+				{Value: 3},
+			},
+		},
+	}
+
+	testMetric(t, want, metric)
+}
+
+func TestProducerMetrics_produced(t *testing.T) {
+	producer, rdr := setupTestProducer(t)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := producer.ProcessBatch(ctx, &model.Batch{
+		model.APMEvent{Transaction: &model.Transaction{ID: "1"}},
+	})
+	assert.NoError(t, err)
+
+	var rm metricdata.ResourceMetrics
+	assert.NoError(t, rdr.Collect(context.Background(), &rm))
+
+	metrics := extractMetrics(rm.ScopeMetrics)
+	assert.Len(t, metrics, 1)
+	metric := metrics[0]
+
+	want := metricdata.Metrics{
+		Name: "message.produced",
+		Data: metricdata.Sum[int64]{
+			DataPoints: []metricdata.DataPoint[int64]{
+				{Value: 1},
+			},
+		},
+	}
+
+	testMetric(t, want, metric)
+}
+
+func extractMetrics(sm []metricdata.ScopeMetrics) []metricdata.Metrics {
+	for _, m := range sm {
+		if m.Scope.Name == instrumentName {
+			return m.Metrics
+		}
+	}
+
+	return []metricdata.Metrics{}
+}
+
+func testMetric(t *testing.T, want metricdata.Metrics, actual metricdata.Metrics) {
+	assert.Equal(t, want.Name, actual.Name)
+
+	wantValue := want.Data.(metricdata.Sum[int64]).DataPoints[0].Value
+	metricValue := actual.Data.(metricdata.Sum[int64]).DataPoints[0].Value
+	assert.Equal(t, wantValue, metricValue)
+}
+
+func setupTestProducer(t *testing.T) (*Producer, sdkmetric.Reader) {
 	rdr := sdkmetric.NewManualReader()
 	mp := sdkmetric.NewMeterProvider(sdkmetric.WithReader(rdr))
-
-	otel.SetMeterProvider(mp)
 
 	topic := apmqueue.Topic("default-topic")
 	_, brokers := newClusterWithTopics(t, topic)
@@ -67,77 +140,5 @@ func TestProducerMetrics(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, producer)
 
-	// NOTE: these tests are flaky
-	// Using go-cmp instead of metricdatatest.AssertEqual allow to bypass
-	// checkes on all metric data fields, which in some cases mey result in
-	// flakiness (i.e. attribute changing value).
-	// But that's not enough to remove all flakiness: as Producer is using 2
-	// meter provider, their order in the rm.ScopeMetrics slice may change,
-	// thus making these tests fails.
-	// You can verify this with:
-	// go test -v -run TestProducerMetrics --tags=metrics -race -count=100 ./kafka/
-
-	// This custom comparer checks for equality only in Name and first
-	// datapoint value.
-	opt := cmp.Comparer(func(x, y metricdata.Metrics) bool {
-		equalName := x.Name == y.Name
-		equalValue := x.Data.(metricdata.Sum[int64]).DataPoints[0].Value ==
-			y.Data.(metricdata.Sum[int64]).DataPoints[0].Value
-		return equalName && equalValue
-
-	})
-
-	t.Run("deadline exceeded", func(t *testing.T) {
-		ctx, cancel := context.WithTimeout(context.Background(), 0*time.Second)
-		defer cancel()
-
-		err = producer.ProcessBatch(ctx, &model.Batch{
-			model.APMEvent{Transaction: &model.Transaction{ID: "1"}},
-		})
-		assert.NoError(t, err)
-
-		var rm metricdata.ResourceMetrics
-		assert.NoError(t, rdr.Collect(context.Background(), &rm))
-
-		metric := rm.ScopeMetrics[1].Metrics[1]
-		want := metricdata.Metrics{
-			Name: "write.timeout.count",
-			Data: metricdata.Sum[int64]{
-				DataPoints: []metricdata.DataPoint[int64]{
-					{
-						Value: 1,
-					},
-				},
-			},
-		}
-
-		assert.True(t, cmp.Equal(want, metric, opt))
-	})
-
-	t.Run("produced", func(t *testing.T) {
-		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-		defer cancel()
-
-		err = producer.ProcessBatch(ctx, &model.Batch{
-			model.APMEvent{Transaction: &model.Transaction{ID: "1"}},
-		})
-		assert.NoError(t, err)
-
-		var rm metricdata.ResourceMetrics
-		assert.NoError(t, rdr.Collect(context.Background(), &rm))
-
-		metric := rm.ScopeMetrics[1].Metrics[0]
-		want := metricdata.Metrics{
-			Name: "message.produced.count",
-			Data: metricdata.Sum[int64]{
-				DataPoints: []metricdata.DataPoint[int64]{
-					{
-						Value: 1,
-					},
-				},
-			},
-		}
-
-		assert.True(t, cmp.Equal(want, metric, opt))
-	})
+	return producer, rdr
 }
