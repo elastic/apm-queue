bef2d93b775c7ef18eaecc064369a60050db7f80
Edoardo Tenani <edoardo.tenani@elastic.co>
2023-08-03 14:19:17 +0200

    tools/queuebench: init
    Add initial implementation of queuebench command.
    
    The command:
    - create a dedicated topic with a unique name (run-<timestamp>)
    - produce a single event; event is a random bytes sequence of given
      length
    - consume a single event
    - print some metrics collected by otel
    - delete created topic
    
    Use a separate go.mod to prevent adding dependency to code depending on
    apm-queue.

---
+++ tools/queuebench/main.go
@@ -0,0 +1,265 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	flag "github.com/spf13/pflag"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/metric/metricdata"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
+
+	apmqueue "github.com/elastic/apm-queue"
+	"github.com/elastic/apm-queue/kafka"
+)
+
+type config struct {
+	brokers    []string
+	duration   time.Duration
+	eventSize  int
+	partitions int
+}
+
+func flags2config() *config {
+	brokers := flag.String("brokers", "", "broker/s bootstrap URL")
+	duration := flag.Int("duration", 0, "duration time in seconds for the benchmark. Required.")
+	eventSize := flag.Int("event-size", 1024, "size of each event, in bytes. Default to 1024B (1KB).")
+	partitions := flag.Int("partitions", 1, "number of partitions to use for benchmarking a single topic. Default: 1.")
+
+	flag.Parse()
+
+	if *brokers == "" {
+		log.Fatal("config validation: brokers must not be empty")
+	}
+	if *partitions != 1 {
+		log.Fatal("config validation: partitions > 1 is not supported (yet)")
+	}
+	if *eventSize != 1024 {
+		log.Fatal("config validation: event-size > 1024 is not supported (yet)")
+	}
+	if *duration == 0 {
+		log.Fatal("config validation: duration must be set and be greater than 0")
+	}
+
+	return &config{
+		brokers:    []string{*brokers},
+		duration:   time.Duration(*duration),
+		eventSize:  *eventSize,
+		partitions: *partitions,
+	}
+}
+
+func main() {
+	cfg := flags2config()
+
+	log.Println("prep logger")
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatal(fmt.Sprintf("cannot create zap logger: %s", err))
+	}
+
+	log.Println("prep MeterProvider")
+	rdr := sdkmetric.NewManualReader()
+	mp := sdkmetric.NewMeterProvider(
+		sdkmetric.WithReader(rdr),
+	)
+
+	kafkaCommonCfg := kafka.CommonConfig{
+		Brokers:   cfg.brokers,
+		Namespace: app,
+
+		Logger:         logger,
+		TracerProvider: trace.NewNoopTracerProvider(),
+		MeterProvider:  mp,
+	}
+
+	log.Println("prep kafka.Manager")
+	mngr, err := newMngr(kafkaCommonCfg, cfg.brokers)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("cannot create kafka manager: %s", err))
+	}
+	defer func() { mngr.Close() }()
+
+	ctx := context.Background()
+
+	if err := mngr.Healthy(ctx); err != nil {
+		log.Fatal(fmt.Sprintf("kafka cluster is not healthy (%s): %s", cfg.brokers, err))
+	}
+
+	log.Println("cluster confirmed healthy")
+
+	ts := time.Now().Unix()
+	topics := []apmqueue.Topic{
+		apmqueue.Topic(fmt.Sprintf("%s-%d", app, ts)),
+	}
+
+	log.Println("creating benchmark kafka topics")
+	if err := createTopics(ctx, mngr, cfg.partitions, topics); err != nil {
+		log.Fatal("cannot create topics: %w", err)
+	}
+	defer deleteTopics(ctx, mngr, topics)
+
+	// generator := newEventGenerator(cfg.duration, cfg.eventSize)
+	event, err := generateEvent(cfg.eventSize)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("cannot generate event: %s", err))
+	}
+
+	producer, err := createProducer(kafkaCommonCfg)
+	if err != nil {
+		log.Fatalf("cannot create kafka producer: %s", err)
+	}
+	defer producer.Close()
+
+	record := apmqueue.Record{
+		Topic: topics[0],
+		// OrderingKey: []byte{},
+		Value: event,
+	}
+	producer.Produce(ctx, record)
+
+	consumer, err := createConsumer(kafkaCommonCfg, topics)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("cannot create consumer: %s", err))
+	}
+	defer consumer.Close()
+
+	c, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	consumer.Run(c)
+
+	var rm metricdata.ResourceMetrics
+	rdr.Collect(context.Background(), &rm)
+
+	fmt.Printf("%+v\n", rm)
+	o := filterMetrics("github.com/twmb/franz-go/plugin/kotel", rm.ScopeMetrics)
+	if len(o) != 0 {
+		for _, m := range o {
+			dps := m.Data.(metricdata.Sum[int64]).DataPoints
+			fmt.Printf("%s @ %s\n", m.Name, dps[0].StartTime)
+
+			for _, dp := range dps {
+				fmt.Printf("%d ", dp.Value)
+				attrs := strings.Builder{}
+				for i, a := range dp.Attributes.ToSlice() {
+					attrs.Write([]byte(fmt.Sprintf("%s:%s", string(a.Key), a.Value.AsString())))
+					if i+1 != len(dp.Attributes.ToSlice()) {
+						attrs.Write([]byte(" "))
+					}
+				}
+				fmt.Printf("(%s)", &attrs)
+				fmt.Println()
+			}
+		}
+		fmt.Printf("\n\n")
+	}
+
+	// go startProducing(producer)
+	// go startConsuming(consumer)
+}
+
+// func startProducing(producer) {
+// 	producer.Start()
+// }
+
+// func startConsuming(consumer) {
+// 	consumer.Start()
+// }
+
+const app = "queuebench"
+
+func newMngr(commoncfg kafka.CommonConfig, brokers []string) (*kafka.Manager, error) {
+	commoncfg.ClientID = fmt.Sprintf("%s-manager", app)
+
+	cfg := kafka.ManagerConfig{
+		CommonConfig: commoncfg,
+	}
+
+	return kafka.NewManager(cfg)
+}
+
+func createTopics(ctx context.Context, mngr *kafka.Manager, partitions int, topics []apmqueue.Topic) error {
+	cfg := kafka.TopicCreatorConfig{
+		PartitionCount: partitions,
+	}
+
+	creator, err := mngr.NewTopicCreator(cfg)
+	if err != nil {
+		return fmt.Errorf("cannot instantiate topic creator: %w", err)
+	}
+
+	for _, topic := range topics {
+		err = creator.CreateTopics(ctx, topic)
+		if err != nil {
+			return fmt.Errorf("cannot create topics: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func deleteTopics(ctx context.Context, mngr *kafka.Manager, topics []apmqueue.Topic) {
+	log.Println("deleting benchmark kafka topics")
+	err := mngr.DeleteTopics(ctx, topics...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createProducer(commoncfg kafka.CommonConfig) (*kafka.Producer, error) {
+	commoncfg.ClientID = fmt.Sprintf("%s-producer", app)
+
+	return kafka.NewProducer(kafka.ProducerConfig{
+		CommonConfig: commoncfg,
+	})
+}
+
+type dummyProcessor struct{}
+
+// Process implements apmqueue.Processor
+func (p dummyProcessor) Process(ctx context.Context, records ...apmqueue.Record) error {
+	fmt.Println(records)
+	return nil
+}
+
+func createConsumer(commoncfg kafka.CommonConfig, topics []apmqueue.Topic) (*kafka.Consumer, error) {
+	commoncfg.ClientID = fmt.Sprintf("%s-consumer", app)
+
+	return kafka.NewConsumer(kafka.ConsumerConfig{
+		CommonConfig: commoncfg,
+
+		Topics:    topics,
+		GroupID:   "zero",
+		Processor: dummyProcessor{},
+	})
+}
+
+func filterMetrics(instrumentName string, sm []metricdata.ScopeMetrics) []metricdata.Metrics {
+	for _, m := range sm {
+		if m.Scope.Name == instrumentName {
+			return m.Metrics
+		}
+	}
+	return []metricdata.Metrics{}
+}
