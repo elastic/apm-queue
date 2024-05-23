1d412d299ab2edce2e154a4f7ac6046519bf0e16
Edoardo Tenani <edoardo.tenani@elastic.co>
2024-05-23 11:22:59 +0200

    only report connects metric
    Do not report 2 separate metrics OnBrokenConnect. Use only connects
    and identify success/failure through 'outcome' dimension.

--- kafka/metrics.go
+++ kafka/metrics.go
@@ -323,19 +323,21 @@ func formatMetricError(name string, err error) error {
 }
 
 func (h *metricHooks) OnBrokerConnect(meta kgo.BrokerMetadata, _ time.Duration, _ net.Conn, err error) {
-	attrs := make([]attribute.KeyValue, 0, 2)
+	attrs := make([]attribute.KeyValue, 0, 3)
 	attrs = append(attrs, semconv.MessagingSystem("kafka"))
 	if h.namespace != "" {
 		attrs = append(attrs, attribute.String("namespace", h.namespace))
 	}
 	if err != nil {
-		h.connectErrs.Add(
+		attrs = append(attrs, attribute.String("outcome", "failure"))
+		h.connects.Add(
 			context.Background(),
 			1,
 			metric.WithAttributeSet(attribute.NewSet(attrs...)),
 		)
 		return
 	}
+	attrs = append(attrs, attribute.String("outcome", "success"))
 	h.connects.Add(
 		context.Background(),
 		1,
