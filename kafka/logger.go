af97614abf1ac4caed60f8d115c91e4e962597f8
inge4pres <francesco.gualazzi@elastic.co>
2024-08-09 17:45:41 +0200

    add broker connection err details
    Signed-off-by: inge4pres <francesco.gualazzi@elastic.co>

--- kafka/logger.go
+++ kafka/logger.go
@@ -16,11 +16,13 @@ type loggerHook struct {
 }
 
 // OnBrokerConnect implements the kgo.HookBrokerConnect interface.
-func (l *loggerHook) OnBrokerConnect(_ kgo.BrokerMetadata, dialDur time.Duration, _ net.Conn, err error) {
+func (l *loggerHook) OnBrokerConnect(meta kgo.BrokerMetadata, dialDur time.Duration, _ net.Conn, err error) {
 	if err != nil {
 		l.logger.Error("failed to connect to broker",
 			zap.Error(err),
 			zap.String("duration", dialDur.String()),
+			zap.String("host", meta.Host),
+			zap.Int32("port", meta.Port),
 		)
 	}
 	return
