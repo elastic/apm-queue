f097d60710e74000b2a1fe663339675ebf7267ff
inge4pres <francesco.gualazzi@elastic.co>
2024-08-09 17:10:13 +0200

    o11y: log errors on broker connect
    Signed-off-by: inge4pres <francesco.gualazzi@elastic.co>

---
+++ kafka/logger.go
@@ -0,0 +1,27 @@
+package kafka
+
+import (
+	"net"
+	"time"
+
+	"github.com/twmb/franz-go/pkg/kgo"
+	"go.uber.org/zap"
+)
+
+// Compile-time check that loggerHook implements the HookBrokerConnect interface.
+var _ kgo.HookBrokerConnect = new(loggerHook)
+
+type loggerHook struct {
+	logger *zap.Logger
+}
+
+// OnBrokerConnect implements the kgo.HookBrokerConnect interface.
+func (l *loggerHook) OnBrokerConnect(_ kgo.BrokerMetadata, dialDur time.Duration, _ net.Conn, err error) {
+	if err != nil {
+		l.logger.Error("failed to connect to broker",
+			zap.Error(err),
+			zap.String("duration", dialDur.String()),
+		)
+	}
+	return
+}
